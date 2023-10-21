4a834f7b48fc751746464f2bd384e76f3cc2e42d
Eric Lee <eric.lee0051@gmail.com>
2023-10-20 21:56:41 -0700

    package edits

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'erleh.github.io/url-shortener',
+  base: 'https://Erleh.github.io/url-shortener',
   plugins: [react()],
 })
